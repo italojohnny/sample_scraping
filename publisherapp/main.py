5b0b8ea031fd247607368616cf584eeebcaaf9b3
italojohnny <italojohnnydosanjos@gmail.com>
2021-05-06 20:24:29 -0300

    modifica publisherapp para consultar cpf no banco

--- publisherapp/main.py
+++ publisherapp/main.py
@@ -2,35 +2,66 @@
 # encoding: utf-8
 import logging
 import pika
+import pymongo
 import time
 import random
 import string
+import os
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
+
 def get_number():
     return ''.join(random.choice(string.digits) for i in range(11))
 
-def test_send_message():
-    logging.info('inicia envio de mensagem')
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-    channel = connection.channel()
+def get_collection():
+    db_address='mongodb'
+    db_port='27017'
+    db_name = os.getenv('MONGO_INITDB_DATABASE')
+    collection_name = os.getenv('MONGO_INITDB_COLLECTION')
+    user_name = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+    user_passwd = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
+    client = pymongo.MongoClient(f'mongodb://{user_name}:{user_passwd}@{db_address}:{db_port}')
+    db = client[db_name]
+    return db[collection_name]
 
-    channel.queue_declare(queue='hello')
-    channel.basic_publish(
-        exchange='',
-        routing_key='hello',
-        body=get_number()
-    )
-    connection.close()
-    logging.info('encerra envio de mensagem')
+def find(number):
+    collection = get_collection()
+    result = [str(i['cpf'], 'utf-8')for i in collection.find({},{ "_id": 0, "cpf": 1 })]
+    logging.error(result)
+    if number in result:
+        return True
+    return False
+
+
+def test_send_message(number):
+    try:
+        logging.error('inicia envio de mensagem')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        channel = connection.channel()
+
+        channel.queue_declare(queue='hello')
+        channel.basic_publish(
+            exchange='',
+            routing_key='hello',
+            body=number
+        )
+        connection.close()
+        logging.error('encerra envio de mensagem')
+    except:
+        logging.exception("Uma falha inesperada ocorreu")
 
 def main():
     time.sleep(5)
     try:
         logging.info('iniciando aplicacao')
         while True:
-            time.sleep(3)
-            test_send_message()
+            number = get_number()
+            if not find(number):
+                logging.error('NENHUM CPF ENCONTRADO')
+                test_send_message(number)
+            else:
+                logging.error('CPF ENCONTRADO')
+            time.sleep(10)
 
     except:
         logging.exception("Uma falha inesperada ocorreu")
