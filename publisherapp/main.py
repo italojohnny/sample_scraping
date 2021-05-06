8efe4e2f7943b7fbb6ae8afc1265c6c253785806
italojohnny <italojohnnydosanjos@gmail.com>
2021-05-06 12:43:11 -0300

    cria funcao que publica no rabbitmq

--- publisherapp/main.py
+++ publisherapp/main.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 import logging
+import pika
 import time
+import random
+import string
 
 logging.basicConfig(level=logging.DEBUG)
+def get_number():
+    return ''.join(random.choice(string.digits) for i in range(11))
+
+def test_send_message():
+    logging.info('inicia envio de mensagem')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(
+        exchange='',
+        routing_key='hello',
+        body=get_number()
+    )
+    connection.close()
+    logging.info('encerra envio de mensagem')
 
 def main():
     time.sleep(5)
@@ -11,7 +30,7 @@ def main():
         logging.info('iniciando aplicacao')
         while True:
             time.sleep(3)
-            logging.info('teste...')
+            test_send_message()
 
     except:
         logging.exception("Uma falha inesperada ocorreu")
