23d0b2c0668759e92c34d77286a2930645df651e
italojohnny <italojohnnydosanjos@gmail.com>
2021-05-06 01:16:27 -0300

    cria codigo para testar conexao entre containers

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 import logging
+from selenium.webdriver import Remote
+import time
 
 logging.basicConfig(level=logging.DEBUG)
 
-
 def main():
-    logging.info("hello world")
+    time.sleep(5)
+    try:
+        logging.info('iniciando aplicacao')
+        browser = Remote(
+            command_executor='http://firefox:4444/wd/hub',
+            desired_capabilities={
+                'browserName':'firefox'
+            }
+        )
+        browser.get('http://google.com')
+        browser.save_screenshot('/tmp/teste.png')
+        browser.quit()
+
+    except:
+        logging.exception("Uma falha inesperada ocorreu")
+
+    finally:
+        logging.info('encerrando aplicacao')
 
 
 if __name__ == '__main__':
