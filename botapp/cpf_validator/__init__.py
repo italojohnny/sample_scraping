cb701b6c3a2c0635f1cf6187472fe96a4df7fbde
italojohnny <italojohnnydosanjos@gmail.com>
2021-05-07 07:52:02 -0300

    remove codigo inutilizado

--- botapp/cpf_validator/__init__.py
+++ botapp/cpf_validator/__init__.py
@@ -72,34 +72,3 @@ class SiteA(Site):
 class SiteB(Site):
     def verify(self, cpf) -> bool:
         return False
-
-
-def is_valid(cpf):
-    result = False
-    logging.error(cpf)
-    try:
-        browser = Remote(
-            command_executor='http://firefox:4444/wd/hub',
-            desired_capabilities={
-                'browserName':'firefox'
-            }
-        )
-        browser.get('https://www.geradorcpf.com/validar-cpf.htm')
-
-        submit_btn = browser.find_element_by_xpath('//*[@id="botaoValidarCPF"]')
-
-        input_text = browser.find_element_by_xpath('//*[@id="cpf"]')
-        input_text.send_keys(str(cpf))
-
-        time.sleep(2)
-        submit_btn.click()
-
-        output = browser.find_element_by_xpath('/html/body/div[3]/section[1]/div/div/div/div[4]/div/span/div').text
-        if re.match(r'( |^)(valid)', output, re.I):
-            result = True
-
-        browser.quit()
-    except:
-        logging.exception('erro inesperado')
-
-    return result
